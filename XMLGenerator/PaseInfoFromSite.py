e444fee158bb0e67593e73ec41e84f89bb0cdb6d
Mindkid <prm.4555@gmail.com>
2016-11-02 22:57:58 +0000

    just updated not working yet
    Signed-off-by: Mindkid <prm.4555@gmail.com>

--- XMLGenerator/PaseInfoFromSite.py
+++ XMLGenerator/PaseInfoFromSite.py
@@ -1,9 +1,22 @@
 import XMLBuilder
+from bs4 import BeautifulSoup
+
+urlglobal = "https://developer.salesforce.com"
+uriInicial = "/docs/atlas.en-us.pages.meta/pages/pages_compref.htm"
 
 class ParseInfoFromSite:
 	def __init__(self):
 		self.XML = XMLBuilder()
 		#TODO
-
+	
+	def parseFunction(self, namespace, funtionName):
+		#TODO
+		self.XML.addFunction(namespace, funtionName)
+	
+	def parseAttribute(self, funtionName, name, type, description, required, access):
+		#TODO
+		self.XML.creatElementToAdd(funtionName, name, type, description, required, access)
+	
+	
 	def finishParser(self):
 		self.writeToFile('visualforce.xml')
